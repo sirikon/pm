f211b52fd12b8a1d00d03aa0ee61215d18f60706
Carlos Fdez. Llamas <hello@sirikon.me>
2021-11-21 18:00:09 +0100

    asdf

--- src/pm/__main__.py
+++ src/pm/__main__.py
@@ -1,9 +1,16 @@
 import click
 
-VERSION='0.1.0'
+VERSION = '0.1.0'
+
 
 @click.group()
 def cli() -> None: pass
+
+
+@cli.command()
+def reindex() -> None:
+    """Recalculate the package index based on the store's contents"""
+    print("reindexin'")
 
 
 @cli.command()

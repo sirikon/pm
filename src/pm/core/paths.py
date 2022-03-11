681412f662a429dcff5855e48c5355f26654d670
Carlos Fdez. Llamas <hello@sirikon.me>
2022-03-11 15:53:45 +0100

    yeet python

--- src/pm/core/paths.py
+++
@@ -1,9 +0,0 @@
-from os import getenv, path
-
-
-def get_root_path() -> str:
-    return getenv('PM_ROOT', path.join(path.expanduser('~'), '.pm'))
-
-
-def get_user_deps_path() -> str:
-    return path.join(get_root_path(), 'user-deps')

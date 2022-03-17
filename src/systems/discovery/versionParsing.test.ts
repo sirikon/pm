b36589d0b277c11c2f1f4e041b2e84d4766865cb
Carlos Fdez. Llamas <hello@sirikon.me>
2022-03-18 00:43:46 +0100

    version parsing

---
+++ src/systems/discovery/versionParsing.test.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "std/testing/asserts.ts";
+import { parseVersionRange } from "./versionParsing.ts";
+
+Deno.test("parseVersionRange works", () => {
+  verifyVersionRange("potato", null);
+  verifyVersionRange("", { from: null, to: null });
+  verifyVersionRange("1.2.3.4", {
+    from: { version: [1, 2, 3, 4], inclusive: true },
+    to: { version: [1, 2, 3, 5], inclusive: false },
+  });
+  verifyVersionRange("1.2", {
+    from: { version: [1, 2, 0, 0], inclusive: true },
+    to: { version: [1, 3, 0, 0], inclusive: false },
+  });
+  verifyVersionRange("1", {
+    from: { version: [1, 0, 0, 0], inclusive: true },
+    to: { version: [2, 0, 0, 0], inclusive: false },
+  });
+});
+
+function verifyVersionRange(
+  data: string,
+  expected: ReturnType<typeof parseVersionRange>,
+) {
+  assertEquals<typeof expected>(parseVersionRange(data), expected);
+}
--- src/systems/discovery/versionParsing.ts
+++ src/systems/discovery/versionParsing.ts
@@ -1,2 +1,32 @@
-export function parseVersionRange(data: string) {
+import { PackageVersion, PackageVersionRange } from "../core/models.ts";
+
+const VERSION_REGEX = /^(?:[0-9]+\.?)+(?<!\.)$/;
+
+export function parseVersionRange(data: string): PackageVersionRange | null {
+  if (data === "") return { from: null, to: null };
+
+  const matchedVersion = matchVersionExpression(data);
+  if (matchedVersion == null) return null;
+  if (matchedVersion.length < 1 || matchedVersion.length > 4) return null;
+
+  const fromVersion = [
+    ...matchedVersion,
+    ...Array(4 - matchedVersion.length).fill(0),
+  ] as PackageVersion;
+
+  const toVersion = [
+    ...matchedVersion.slice(0, matchedVersion.length - 1),
+    matchedVersion[matchedVersion.length - 1] + 1,
+    ...Array(4 - matchedVersion.length).fill(0),
+  ] as PackageVersion;
+
+  return {
+    from: { version: fromVersion, inclusive: true },
+    to: { version: toVersion, inclusive: false },
+  };
+}
+
+function matchVersionExpression(data: string): number[] | null {
+  if (!VERSION_REGEX.test(data)) return null;
+  return data.split(".").map((i) => parseInt(i));
 }

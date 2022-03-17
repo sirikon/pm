db61306697a0d9ad793a14c27914d085de881025
Carlos Fdez. Llamas <hello@sirikon.me>
2022-03-17 16:13:41 +0100

    some models

--- src/systems/core/models.ts
+++ src/systems/core/models.ts
@@ -1,5 +1,22 @@
-export type Package = {
+type PackageBase = {
   name: string;
+  variant: string;
+};
+export type Package = PackageBase & {
   version: PackageVersion;
 };
 export type PackageVersion = [number, number, number, number];
+
+export type PackageRange = PackageBase & {
+  versionRange: PackageVersionRange;
+};
+export type PackageVersionRange = {
+  from: {
+    version: PackageVersion;
+    inclusive: boolean;
+  } | null;
+  to: {
+    version: PackageVersion;
+    inclusive: boolean;
+  } | null;
+};

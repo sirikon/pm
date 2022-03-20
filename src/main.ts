54cdb4eead49ab961e2b56892beffeb7c1261d3a
Carlos Fdez. Llamas <hello@sirikon.me>
2022-03-20 23:46:44 +0100

    add pmroot

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 import { getExplicitPackageRanges } from "./systems/discovery/mod.ts";
+import { join } from "std/path/mod.ts";
+import { ensureDir } from "std/fs/mod.ts";
+import { DB } from "sqlite/mod.ts";
+
+const pmRoot = Deno.env.get("PM_ROOT") || "/pm";
+await ensureDir(pmRoot);
+
+const db = new DB(join(pmRoot, "db"));
+db.query(`
+  CREATE TABLE IF NOT EXISTS versions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+  )
+`);
 
 console.log(JSON.stringify(await getExplicitPackageRanges(), null, 2));
